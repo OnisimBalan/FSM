065a5099f864f2597bb5eac62a28661d220509f8
OnisimBalan <onisim.balan99@e-uvt.ro>
2025-03-19 12:55:50 +0200

    #5

--- backend/be_FSM/obj/Debug/net9.0/be_FSM.AssemblyInfo.cs
+++ backend/be_FSM/obj/Debug/net9.0/be_FSM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("be_FSM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6682f636b0da3facb6ec26fdbfaeef1a47b8768")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7aeae8243c37d408865073727f568d85c7a80d7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("be_FSM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("be_FSM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
